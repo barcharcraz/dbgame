afb4813ac14196d8e9c337e30c41c5d53615c0c4
Charlie Barto <bartoc@umich.edu>
2016-08-01 02:20:18 -0400

    more work on dbgame

--- pysrc/dbgame/ui/main.py
+++ pysrc/dbgame/ui/main.py
@@ -1,10 +1,12 @@
-from PyQt5.QtCore import QCoreApplication, QUrl
+from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
+from . import DgbameQueryModel
 import sys
 import os
 
 apppath = os.path.dirname(__file__)
 
-app = QCoreApplication(sys.argv)
-engine = QQmlApplicationEngine(QUrl("qrc:/qml/main.qml"))
-app.exec()
+app = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine(app)
+engine.load("dbgame/ui/qml/qml.qml")
+sys.exit(app.exec_())
